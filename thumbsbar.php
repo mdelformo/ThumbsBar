7048d5ab30bf493de6bd22ab39271530a567c25d
Marcus Persson <marcus@delformo.se>
2011-05-24 21:18:26 +0200

    file metadata: i.e. |test.png,0,212|test2.jpg,212,50

--- thumbsbar.php
+++ thumbsbar.php
@@ -1,123 +1,117 @@
 <?php
 
-$tb 	= new ThumbsBar();
+$tb = new ThumbsBar();
 
-$req	= $_GET["q"];
+$req = $_GET["q"];
 
 if ($req == "create") {
 
-	$tb->createThumbs(200, 100);
-	
-	echo "Thumbnails created";
-		
+    $tb->createThumbs(200, 100);
+
+    echo "Thumbnails created";
 }
 
 if ($req == "show") {
 
-	echo $tb->getThumbs();
-
+    echo $tb->getThumbs();
 }
-
 
 class ThumbsBar {
 
-	private $dir;
-	private $files;
-	private $imgs;
+    private $dir;
+    private $files;
 
-	function ThumbsBar($dir = 'img/') {
+//    private $imgs;
 
-		$this->dir 		 = $dir;
-		$this->files	 = $this->getImages($this->dir);
-		
-	}
+    function ThumbsBar($dir = 'img/') {
 
-	public function createThumbs($width, $height) {
-		
-		$total_width 	= 0;
-		$thumbs			= array();
-		
-		// For each image file, create a thumbnail and save it to an array
-		
-		foreach ($this->files as $file) {
-		
-			$thumb = new Imagick($this->dir . $file);
-			$thumb->thumbnailImage($width, $height, true);
-			
-			$thumbs[] = $thumb;
-			
-			$total_width += $thumb->getImageWidth();
-			
-			
-		}
-		
-		// Create a new image file with width = sum of all thumbs
-		
-		$thumbsbar = new Imagick();
-		$thumbsbar->newImage($total_width, $height, new ImagickPixel('black'));
-		$thumbsbar->setImageFormat('jpeg');
-		$thumbsbar->setImageProperty('Xpos', 'testing!');
-		
-		$thumbx = 0;
-		
-		// Append every thumb to the new image file
-		
-		foreach ($thumbs as $thumb) {
-		
-			$thumbsbar->compositeImage($thumb, imagick::COMPOSITE_OVER, $thumbx, 0);
-			
-			$thumbx += $thumb->getImageWidth();
-		
-		}
-		
-		// Write thumbnails to file
-		
-		try {
-		
-			$thumbsbar->writeImage($this->dir . 'thumbsbar.jpg');
-			
-		
-		} catch (Exception $e) {
-		
-			echo 'Failed to write to ' .$e->getMessage();
-		
-		}
-		
-	}
+        $this->dir = $dir;
+        $this->files = $this->getImages($this->dir);
+    }
 
-	public function getThumbs() {
+    public function createThumbs($width, $height) {
 
-		$thumbsbar = new Imagick('img/thumbsbar.jpg');
-		return $thumbsbar->getImageProperty('Xpos');
-		
-	}
+        $total_width = 0;
+        $thumbs = array();
 
-	private function getImages($source) {
+        // For each image file, create a thumbnail and save it to an array
 
-		$dir		= scandir($source);
-		$img_files	= array();
+        foreach ($this->files as $file) {
 
-		foreach ($dir as $file) {
+            $thumb = new Imagick($this->dir . $file);
+            $thumb->thumbnailImage($width, $height, true);
+            
+            // Workaround. Error in Imagick getFilename()
+            $thumb->setFilename($file);
+            $thumbs[] = $thumb;
 
-			if ($this->isImage($file) && $file != 'thumbsbar.jpg') {
-			
-				$img_files[] = $file;
-				
-			}
+            $total_width += $thumb->getImageWidth();
+        }
 
-		}
-		
-		return $img_files;
+        // Create a new image file with width = sum of all thumbs
 
-	}
-	
-	private function isImage($file) {
-		
-		// \z = is last in string, /i = ignore case
-		if (preg_match('/jpg|jpeg|png|gif\z/i', $file)) return true;
-		else return false;
+        $thumbsbar = new Imagick();
+        $thumbsbar->newImage($total_width, $height, new ImagickPixel('black'));
+        $thumbsbar->setImageFormat('jpeg');
 
-	}
+        $thumbx = 0;
+
+        // Append every thumb to the new image file
+        $comment = '';
+        foreach ($thumbs as $thumb) {
+            $comment .= '|' . $thumb->getFilename() . ',';
+            $thumbsbar->compositeImage($thumb, imagick::COMPOSITE_OVER, $thumbx, 0);
+            $comment .= $thumbx . ',';
+            $thumbx += $thumb->getImageWidth();
+            $comment .= $thumb->getImageWidth();
+        }
+        
+        // Comment will look like:
+        // |android.png,0,100|ios.gif,100,54,blackberry.png,154,70
+
+        $thumbsbar->setImageProperty('comment', $comment);
+
+        // Write thumbnails to file
+
+        try {
+
+            $thumbsbar->writeImage($this->dir . 'thumbsbar.jpg');
+        } catch (Exception $e) {
+
+            echo 'Failed to write to ' . $e->getMessage();
+        }
+    }
+
+    public function getThumbs() {
+
+        $thumbsbar = new Imagick('img/thumbsbar.jpg');
+        return $thumbsbar->getImageProperty('comment');
+    }
+
+    private function getImages($source) {
+
+        $dir = scandir($source);
+        $img_files = array();
+
+        foreach ($dir as $file) {
+
+            if ($this->isImage($file) && $file != 'thumbsbar.jpg') {
+
+                $img_files[] = $file;
+            }
+        }
+
+        return $img_files;
+    }
+
+    private function isImage($file) {
+
+        // \z = is last in string, /i = ignore case
+        if (preg_match('/jpg|jpeg|png|gif\z/i', $file))
+            return true;
+        else
+            return false;
+    }
 
 }
 
